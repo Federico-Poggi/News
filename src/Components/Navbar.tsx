741b5c885e5d0e2ff7e471ebdda0a055b9673515
federicopoggi <138136326+federicopoggi@users.noreply.github.com>
2023-11-09 16:18:05 +0100

    second

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,23 @@
-import {Container, FormControl, InputGroup, Navbar, NavItem, Row} from "react-bootstrap";
+import {Button, Container, FormControl, InputGroup, Navbar, NavItem, Row} from "react-bootstrap";
+import {useState} from "react";
+import {isBooleanObject} from "util/types";
 
 
 const NavbarMain=()=>{
-
+    const [clicked, setClicked ] = useState<boolean>(false)
     return(
         <Container fluid>
             <Row>
-                <Navbar className={'d-flex bg-dark'}>
-                <h2 className={ 'h2 fw-bold  text-light py-3'}>NewsWord</h2>
-                    <ul className={'d-flex align-items-center'}>
+                <Navbar className={'d-flex bg-dark '}>
+                <h2 className={ 'h2 fw-bold  text-light py-3 px-4'}>NewsWord</h2>
+                    <ul className={'d-flex'}>
                     <NavItem className={'px-3 text-light'}>Home</NavItem>
                     <NavItem className={'px-3 text-light'}>Home</NavItem>
                     <NavItem className={'px-3 text-light'}>Home</NavItem>
                     </ul>
                 <InputGroup className={'w-25 ms-auto px-3'}>
-                    <FormControl type={'search'} placeholder={'Search News '} />
+                    <FormControl type={'search'} placeholder={'Search News '} className={'bg-transparent border-black text-light'} />
+                    <Button className={'btn nav-pills bg-transparent border-black'} onClick={()=>setClicked(true)}>Search</Button>
                 </InputGroup>
                 </Navbar>
             </Row>
